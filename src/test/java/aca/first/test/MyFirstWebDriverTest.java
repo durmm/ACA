89b49045b8aa06102e35075988ce862971a5a488
durmm <gaya.stdev@gmail.com>
2017-04-08 10:51:08 +0400

    add new test with the test case

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,13 +1,15 @@
 package aca.first.test;
 
 import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class MyFirstWebDriverTest {
 	
-	@Test()
+	/*@Test()
 	public void myFirstTest(){
 		System.setProperty("webdriver.gecko.driver", "/home/transparency/geckodriver");
 		
@@ -16,5 +18,27 @@ public class MyFirstWebDriverTest {
 		 driver.close();
 		 driver.quit();
 	}
+	*/
+	
+	/*
+	 Test Case 1: Verify that URL opens the correct web-page
+	 Summary: The http://winekloud.codebnb.me URL should open the projects main landing page
+	 Steps: 1. Navigate to the http://winekloud.codebnb.me URL.
+	 Expected Result: It should open the main landing page of the WineKloud project.
+	 */
+	
+	@Test()
+	public void checkLandingPage(){
+		System.setProperty("webdriver.gecko.driver", "/home/transparency/geckodriver");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://winekloud.codebnb.me");
+		Assert.assertTrue("The Daily Wine Deal Marketplace", true);
+		driver.findElement(By.xpath("//a[@class='showDealsBtn']"));
+		driver.findElement(By.xpath("//div[@class='bottomNav']"));
+		driver.quit();
+		
+	}
 
 }
+
