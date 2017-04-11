430064154ca45ce41451a1023721721decb7417f
durmm <gaya.stdev@gmail.com>
2017-04-11 17:02:27 +0400

    case 2 update

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,16 +1,15 @@
 package aca.first.test;
 
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class MyFirstWebDriverTest {
-	
-	System.setProperty("webdriver.gecko.driver", "/home/transparency/geckodriver");
-	WebDriver driver = new FirefoxDriver();
 	
 	/*@Test()
 	public void myFirstTest(){
@@ -21,17 +20,16 @@ public class MyFirstWebDriverTest {
 		 driver.close();
 		 driver.quit();
 	}
-	*/
 	
-	/*
 	 Test Case 1: Verify that URL opens the correct web-page
 	 Summary: The http://winekloud.codebnb.me URL should open the projects main landing page
 	 Steps: 1. Navigate to the http://winekloud.codebnb.me URL.
 	 Expected Result: It should open the main landing page of the WineKloud project.
 	 */
 	
-	@Test()
+	/*@Test()
 	public void checkLandingPage(){
+		System.setProperty("webdriver.gecko.driver", "/home/transparency/geckodriver");
 		
 		driver.get("http://winekloud.codebnb.me");
 		Assert.assertTrue("The Daily Wine Deal Marketplace", true);
@@ -41,6 +39,7 @@ public class MyFirstWebDriverTest {
 		driver.quit();
 		
 	}
+	*/
 	
 	/*
 	 Test Case 2: Verify that Deal Kloud is available from the landing page.
@@ -49,14 +48,34 @@ public class MyFirstWebDriverTest {
 	 		2. Click on the "Show me the Deals!" button.
 	 Expected Result: Deal Kloud page should be opened.
 	 */
-	
+	WebDriver driver ;
+	@BeforeSuite()
+	private void driverInit(){
+		System.setProperty("webdriver.gecko.driver", "/home/transparency/geckodriver");
+		 driver = new FirefoxDriver();
+	}
 	@Test
 	public void dealKloudViaHome() {
+		
 		driver.get("http://winekloud.codebnb.me");
 		driver.findElement(By.xpath("//a[@class='showDealsBtn']")).click();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
 		//String elementClass = driver.findElement(By.text("Deal Kloud")).getAttribute(class);
-		//Assert.assertTrue("error", elementClass = "active");
-		Assert.assertEquals("With over 300 brand new Deals every day from more than 1,000 Wine Retailers,", driver.findElement(By.xpath(//p[@contains='With over 300 brand new Deals every day from more than 1,000 Wine Retailers,'])));
+		//WebElement elem = driver.findElement(By.linkText("Deal Kloud"));
+		Assert.assertEquals("active", driver.findElement(By.linkText("Deal Kloud")).getAttribute("class"));
+		//String l = driver.findElement(By.xpath("//p[@class='hero_txt']")).getText();
+		//System.out.println(l);
+		String pText = driver.findElement(By.xpath("//p[@class ='hero_txt']")).getText();
+		boolean textFound = pText.contains("With over 300 brand new Deals every day from more than 1,000 Wine Retailers,");
+		Assert.assertTrue(textFound);
+//		Assert.assertEquals.getText()'
+		driver.close();
 		driver.quit();
 	}
 
